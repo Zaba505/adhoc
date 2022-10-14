43f40f444e3d8f0a135894e581262cb29a0cf9c2
zaba505 <cakub6@gmx.com>
2022-10-14 01:39:24 -0400

    feat(graphs): nearly completed topological sort algorithm

--- graphs/topological/sort.go
+++ graphs/topological/sort.go
@@ -8,6 +8,120 @@ import (
 
 var ErrUnsortable = errors.New("topological: graph is not sortable")
 
+// Sort
 func Sort(g *model.Graph) ([]*model.Subject, error) {
-	return nil, nil
+	triples := toSet(g.Triples)
+
+	var L []*model.Subject
+	S := getStartingNodes(triples)
+
+	for len(S) > 0 {
+		n := popFirst(S)
+		L = append(L, n)
+
+		nEdges := getOutgoingEdges(n, triples)
+		for _, e := range nEdges {
+			delete(triples, e)
+
+			o := e.Object
+			os, isSubject := o.Value.(*model.Object_Subject)
+			if !isSubject {
+				continue
+			}
+
+			m := os.Subject
+			mEdges := getIncomingEdges(m, triples)
+			if len(mEdges) > 1 {
+				continue
+			}
+
+			S[m] = struct{}{}
+		}
+	}
+
+	if len(triples) > 0 {
+		return nil, ErrUnsortable
+	}
+
+	return L, nil
+}
+
+func toSet[T comparable](ts []T) map[T]struct{} {
+	m := make(map[T]struct{})
+	for _, t := range ts {
+		m[t] = struct{}{}
+	}
+	return m
+}
+
+func getStartingNodes(triples map[*model.Triple]struct{}) map[*model.Subject]struct{} {
+	m := make(map[*model.Subject]struct{})
+
+	// Identify all subjects
+	for t := range triples {
+		s := t.Subject
+		m[s] = struct{}{}
+
+		o := t.Object
+		os, isSubject := o.Value.(*model.Object_Subject)
+		if !isSubject {
+			continue
+		}
+		s = os.Subject
+		m[s] = struct{}{}
+	}
+
+	// Remove all subjects which have an edge pointing towards them
+	for t := range triples {
+		val := t.Object.Value
+		os, isSubject := val.(*model.Object_Subject)
+		if !isSubject {
+			continue
+		}
+
+		s := os.Subject
+		delete(m, s)
+	}
+	return m
+}
+
+func popFirst[T comparable](set map[T]struct{}) T {
+	var val T
+	for x := range set {
+		val = x
+		break
+	}
+	delete(set, val)
+	return val
+}
+
+// getIncomingEdges returns all the incoming edges to the given node
+func getIncomingEdges(n *model.Subject, triples map[*model.Triple]struct{}) []*model.Triple {
+	var nTriples []*model.Triple
+	for t := range triples {
+		o := t.Object
+		os, isSubject := o.Value.(*model.Object_Subject)
+		if !isSubject {
+			continue
+		}
+
+		s := os.Subject
+		if n != s {
+			continue
+		}
+		nTriples = append(nTriples, t)
+	}
+	return nTriples
+}
+
+// getOutgoingEdges returns all the outgoing edges from the given node
+func getOutgoingEdges(n *model.Subject, triples map[*model.Triple]struct{}) []*model.Triple {
+	var nTriples []*model.Triple
+	for t := range triples {
+		if t.Subject != n {
+			continue
+		}
+		nTriples = append(nTriples, t)
+	}
+	return nTriples
 }
--- graphs/topological/sort_test.go
+++ graphs/topological/sort_test.go
@@ -10,15 +10,9 @@ import (
 
 func TestSort(t *testing.T) {
 	t.Run("will fail to sort", func(t *testing.T) {
-		t.Run("if the graph is not a directed acyclic graph", func(t *testing.T) {
-			subA := &model.Subject{
-				Type: "Node",
-				Tuid: "A",
-			}
-			subB := &model.Subject{
-				Type: "Node",
-				Tuid: "B",
-			}
+		t.Run("if the graph contains a cycle", func(t *testing.T) {
+			subA := node("Node", "A")
+			subB := node("Node", "B")
 
 			a := withSubject(subA)
 			b := withSubject(subB)
@@ -40,6 +34,77 @@ func TestSort(t *testing.T) {
 			}
 		})
 	})
+
+	t.Run("will succeed to sort", func(t *testing.T) {
+		t.Run("if the graph is made of only attribute edges", func(t *testing.T) {
+			nodeA := node("Node", "A")
+
+			a := withSubject(nodeA)
+
+			g := &model.Graph{
+				Triples: []*model.Triple{
+					a(pred("type"), str(nodeA.Type)),
+					a(pred("type_id"), str(nodeA.Tuid)),
+				},
+			}
+
+			sortedNodes, err := Sort(g)
+			if !assert.Nil(t, err) {
+				return
+			}
+			if !assert.Equal(t, 1, len(sortedNodes)) {
+				return
+			}
+		})
+
+		t.Run("if the graph contains multiple nodes", func(t *testing.T) {
+			nodeA := node("Node", "A")
+			nodeB := node("Node", "B")
+			nodeC := node("Node", "C")
+			nodeD := node("Node", "D")
+
+			a := withSubject(nodeA)
+			b := withSubject(nodeB)
+			c := withSubject(nodeC)
+			d := withSubject(nodeD)
+
+			g := &model.Graph{
+				Triples: []*model.Triple{
+					a(pred("type"), str(nodeA.Type)),
+					a(pred("type_id"), str(nodeA.Tuid)),
+					a(pred("friend"), subject(nodeB)),
+					a(pred("friend"), subject(nodeC)),
+
+					b(pred("type"), str(nodeB.Type)),
+					b(pred("type_id"), str(nodeB.Tuid)),
+					b(pred("friend"), subject(nodeD)),
+
+					c(pred("type"), str(nodeC.Type)),
+					c(pred("type_id"), str(nodeC.Tuid)),
+					c(pred("friend"), subject(nodeD)),
+
+					d(pred("type"), str(nodeD.Type)),
+					d(pred("type_id"), str(nodeD.Tuid)),
+				},
+			}
+
+			sortedNodes, err := Sort(g)
+			if !assert.Nil(t, err) {
+				return
+			}
+			if !assert.Equal(t, 4, len(sortedNodes)) {
+				t.Log(sortedNodes)
+				return
+			}
+		})
+	})
+}
+
+func node(typ, tuid string) *model.Subject {
+	return &model.Subject{
+		Type: typ,
+		Tuid: tuid,
+	}
 }
 
 func withSubject(s *model.Subject) func(*model.Predicate, *model.Object) *model.Triple {
