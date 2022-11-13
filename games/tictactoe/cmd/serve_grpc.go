1371265e535c8718b48c0dac3b5120c904b7c2ce
zaba505 <cakub6@gmx.com>
2022-11-12 19:47:10 -0500

    feat(game/tictactoe): tictactoe service can now be served via the cli

--- games/tictactoe/cmd/serve_grpc.go
+++ games/tictactoe/cmd/serve_grpc.go
@@ -6,8 +6,14 @@
 package cmd
 
 import (
+	"errors"
+	"net"
+
+	"github.com/Zaba505/adhoc/games/tictactoe/grpc"
+
 	"github.com/spf13/cobra"
 	"github.com/spf13/viper"
+	"go.uber.org/zap"
 )
 
 func withServeGrpcCmd() func(*viper.Viper) *cobra.Command {
@@ -19,7 +25,30 @@ func withServeGrpcCmd() func(*viper.Viper) *cobra.Command {
 				loadConfigFile(v),
 			)(v),
 			Run: func(cmd *cobra.Command, args []string) {
+				addr := v.GetString("addr")
+				ls, err := net.Listen("tcp", addr)
+				if err != nil {
+					zap.L().Fatal(
+						"unexpected error when trying to listen on address",
+						zap.String("addr", addr),
+						zap.Error(err),
+					)
+					return
+				}
+				zap.L().Info("listening for grpc requests", zap.String("addr", addr))
 
+				tictactoe := grpc.NewTicTacToeService(
+					grpc.WithLogger(zap.L()),
+				)
+				err = tictactoe.Serve(cmd.Context(), ls)
+				if err != nil && !errors.Is(err, grpc.ErrServerStopped) {
+					zap.L().Fatal(
+						"unexpected error when serving grpc traffic",
+						zap.String("addr", addr),
+						zap.Error(err),
+					)
+					return
+				}
 			},
 		}
 
